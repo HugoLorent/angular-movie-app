caed65d8e5abecc88a2b8932b8035ee5e5af41b3
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-03-03 14:19:28 +0100

    add languages and genres
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../services/movie.service';
+import { Genre } from '../types/genre';
 import { Movie } from '../types/movie';
 
 @Component({
@@ -10,14 +11,19 @@ import { Movie } from '../types/movie';
 })
 export class MovieDetailsComponent implements OnInit {
   movie: Movie;
+  genres: Genre[];
 
-  constructor(private movieServie: MovieService, private route: ActivatedRoute) {}
+  constructor(
+    private movieServie: MovieService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.movieServie.getMovie(id).subscribe({
       next: (movie) => {
         this.movie = movie;
+        this.genres = this.movie.genres;
         console.log(movie);
       },
       error: (error) => {
--- src/app/types/movie.ts
+++ src/app/types/movie.ts
@@ -1,5 +1,14 @@
+import { Genre } from './genre';
+import { Language } from './language';
+
 export class Movie {
   id: number;
   title: string;
   poster_path: string;
+  tagline: string;
+  vote_average: string;
+  runtime: number;
+  release_date: string;
+  spoken_languages: Language[];
+  genres: Genre[];
 }
